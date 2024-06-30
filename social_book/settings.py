3064fdb13910ca818ac3fc0f3a8972632c9c3c35
Subhash <98774745+letsbegin1234@users.noreply.github.com>
2024-06-30 13:48:35 +0530

    d

--- social_book/settings.py
+++ social_book/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,10 +76,14 @@ WSGI_APPLICATION = 'social_book.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    #    {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'telusko',
+    #     'USER': 'postgres',
+    #     'PASSWORD':'1234',
+    #     'HOST':'localhost'
+    # }
 }
 
 
